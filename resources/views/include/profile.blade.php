51dd897399bc7450c32ce33f1d819bae8df7a3de
rikoansh <riko.anshori@gmail.com>
2015-12-21 14:28:45 +0800

    Revert "Design v1.1"
    This reverts commit 997356f37d2c53aefd4b7db601ba6ae7a5fb673b.

--- resources/views/include/profile.blade.php
+++ resources/views/include/profile.blade.php
@@ -1,8 +1,11 @@
 <a class="dropdown-toggle" data-toggle="dropdown" href="#">
     <i class="fa fa-user fa-3x"></i>
                     </a>
-                    <!-- dropdown user-->
+
                     <ul class="dropdown-menu dropdown-user">
+                        <li><a href="{{route('admin::setting')}}"><i class="fa fa-sign-out fa-fw"></i>Setting Profile</a>
+                        </li>
+                        <li class="divider"></li>
                         <li><a href="{{route('getLogout')}}"><i class="fa fa-sign-out fa-fw"></i>Logout</a>
                         </li>
                     </ul>
