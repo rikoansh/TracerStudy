6937e85a7f687063f51d531489aec8ecfbbfb387
Ryan Maulana <iyaryan22@gmail.com>
2015-12-24 18:29:24 +0800

    Test 1

---
+++ resources/views/alumni/berita.blade.php
@@ -0,0 +1,82 @@
+@extends('_layout/basealumni')
+
+@section('title','Home')
+
+@section('konten')
+
+<div class="panel-body">
+
+  <div class="row">
+  <main id="main" class="site-main" role="main">
+			
+    <article>
+    
+    <header class="entry-headers col-md-12">
+    <div class="entry-meta">
+    <span class="posted-on">Posted on August 19, 2013 by admin</span>
+    </div><!-- .entry-meta -->
+                    
+    <h1 class="entry-titles">Etiam mauris tortor, pharetra quis lobortis in, pharetra in diam</h1>
+    </header><!-- .entry-header -->
+    
+    <div class="col-sm-6 post-image">							
+    <a href="#"> <img class="img-responsive" src="{{ asset('themes/img/2015-11-09_172056.png')}}"></a>
+    </div>
+    <div class="entry-summary col-sm-6">
+    <p>Duis tempus leo vitae ipsum viverra, blandit condimentum sapien porttitor. Duis porttitor sed metus eget mollis. Curabitur bibendum imperdiet tortor, ut pulvinar purus elementum nec. Suspendisse at erat luctus; hendrerit sapien sed, consectetur erat. Maecenas dignissim suscipit orci at molestie. Aenean fringilla dolor vitae lacus lacinia eleifend. Suspendisse et libero nunc. Etiam mauris tortor, pharetra […]</p>
+    <a class="btn  btn-primary" href="#"> Read More </a>
+    </div><!-- .entry-summary -->
+        
+    <div class="clearfix"></div>
+    </article><!-- #post-## -->
+    
+    <article>
+    
+    <header class="entry-headers col-md-12">
+    <div class="entry-meta">
+    <span class="posted-on">Posted on August 19, 2013 by admin</span>
+    </div><!-- .entry-meta -->
+                    
+    <h1 class="entry-titles">Etiam mauris tortor, pharetra quis lobortis in, pharetra in diam</h1>
+    </header><!-- .entry-header -->
+    
+    <div class="col-sm-6 post-image">							
+    <a href="#"> <img class="img-responsive" src="{{ asset('themes/img/2015-11-09_172056.png')}}"></a>
+    </div>
+    <div class="entry-summary col-sm-6">
+    <p>Duis tempus leo vitae ipsum viverra, blandit condimentum sapien porttitor. Duis porttitor sed metus eget mollis. Curabitur bibendum imperdiet tortor, ut pulvinar purus elementum nec. Suspendisse at erat luctus; hendrerit sapien sed, consectetur erat. Maecenas dignissim suscipit orci at molestie. Aenean fringilla dolor vitae lacus lacinia eleifend. Suspendisse et libero nunc. Etiam mauris tortor, pharetra […]</p>
+    <a class="btn  btn-primary" href="#"> Read More </a>
+    </div><!-- .entry-summary -->
+        
+    <div class="clearfix"></div>
+    </article><!-- #post-## -->
+    
+    <article>
+    
+    <header class="entry-headers col-md-12">
+    <div class="entry-meta">
+    <span class="posted-on">Posted on August 19, 2013 by admin</span>
+    </div><!-- .entry-meta -->
+                    
+    <h1 class="entry-titles">Etiam mauris tortor, pharetra quis lobortis in, pharetra in diam</h1>
+    </header><!-- .entry-header -->
+    
+    <div class="col-sm-6 post-image">							
+    <a href="#"> <img class="img-responsive" src="{{ asset('themes/img/2015-11-09_172056.png')}}"></a>
+    </div>
+    <div class="entry-summary col-sm-6">
+    <p>Duis tempus leo vitae ipsum viverra, blandit condimentum sapien porttitor. Duis porttitor sed metus eget mollis. Curabitur bibendum imperdiet tortor, ut pulvinar purus elementum nec. Suspendisse at erat luctus; hendrerit sapien sed, consectetur erat. Maecenas dignissim suscipit orci at molestie. Aenean fringilla dolor vitae lacus lacinia eleifend. Suspendisse et libero nunc. Etiam mauris tortor, pharetra […]</p>
+    <a class="btn  btn-primary" href="#"> Read More </a>
+    </div><!-- .entry-summary -->
+        
+    <div class="clearfix"></div>
+    </article><!-- #post-## -->
+    
+    </main>
+
+    </div>
+  </div>
+
+@stop
+
+
